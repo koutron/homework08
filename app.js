521dbbc259cc5cd49644b34753859543b5e4b56a
Kouros <kamousavi@gmail.com>
2020-04-28 23:15:35 -0400

    added inquirer prompts for manager, engineer, and intern

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+//const Manager = require("./lib/Manager");
+//const Engineer = require("./lib/Engineer");
+//const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -19,71 +19,222 @@ const idArray = [];
 
 
 // STUDENT: This function generates all the questions for creating the manager. You need to add more to this.
-function createManager(){
-  console.log("Please build your team");
-  inquirer.prompt([
-    {
-      type: "input",
-      name: "managerName",
-      message: "What is your manager's name?",
-      // Note how the validate function works
-      validate: answer => {
-        if (answer !== "") {
-          return true;
-        }
-        return "Please enter at least one character.";
-      }
-    },
-
-    // STUDENT: Add other questions here!
+function createManager() {
+    console.log("Please build your team");
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "managerName",
+            message: "What is your manager's name?",
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Please enter at least one character.";
+            }
+        },
+        {
+            type: "input",
+            name: "managerId",
+            message: "What is your manager's ID?",
+            validate: answer => {
+                if (!idArray.includes(answer)) {
+                    return true;
+                }
+                return "Please enter a unique ID.";
+            }
+        },
+        {
+            type: "input",
+            name: "managerEmail",
+            message: "What is your manager's email?",
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Please enter at least one character.";
+            }
+        },
+        {
+            type: "input",
+            name: "managerOfficeNum",
+            message: "What is your manager's office number?",
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Please enter at least one character.";
+            }
+        },
+        // STUDENT: Add other questions here!
 
 
     ]).then(answers => {
-      // STUDENT: Process the response by instatiating a new object in the Manager class
+        // STUDENT: Process the response by instatiating a new object in the Manager class
+        idArray.push(answers.managerId);
 
-
-      // Now call the next question set
-      createTeam();
+        // Now call the next question set
+        createTeam();
     });
 }
 
 // This function starts team creation.
 function createTeam() {
-  inquirer.prompt([
     // STUDENT: Ask which type of team member should be created with a list of choices
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "Who would you like to add to your team?",
+            name: "teamChoice",
+            choices: ["Engineer", "Intern", "Team Complete"]
+        }
 
-  ]).then(userChoice => {
-    // STUDENT: Based on which choice they make, call the correct function to ask more questions.
-    // If no choice is made, then go to the rendering function.
+    ]).then(userChoice => {
+        // STUDENT: Based on which choice they make, call the correct function to ask more questions.
+        // If no choice is made, then go to the rendering function.
+        if (userChoice.teamChoice === "Engineer") {
+            createEngineer();
+        } else if (userChoice.teamChoice === "Intern") {
+            createIntern();
+        } else {
+            // Call render function correctly and pass in team array
+        }
 
-
-  });
+    });
 }
 
 // This function starts team creation.
 function createEngineer() {
-  inquirer.prompt([
-    // STUDENT:  Engineer questions
+    inquirer.prompt([
+        // STUDENT:  Engineer questions
+        {
+            type: "input",
+            name: "engineerName",
+            message: "What is your engineer's name?",
+            // Note how the validate function works
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Please enter at least one character.";
+            }
+        },
+        {
+            type: "input",
+            name: "engineerId",
+            message: "What is your engineer's ID?",
+            // Note how the validate function works
+            validate: answer => {
+                if (!idArray.includes(answer)) {
+                    return true;
+                }
+                return "Please enter a unique ID.";
+            }
+        },
+        {
+            type: "input",
+            name: "engineerEmail",
+            message: "What is your engineer's email?",
+            // Note how the validate function works
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Please enter at least one character.";
+            }
+        },
+        {
+            type: "input",
+            name: "engineerGithub",
+            message: "What is your engineer's Github username?",
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Please enter at least one character.";
+            }
+        }
+    ]).then(answers => {
+        idArray.push(answers.engineerId);
+        // STUDENT: Make sure the id supplied is unique, then take the data supplied and 
+        // instantiate the Engineer constructor.
 
-  ]).then(userChoice => {
-    // STUDENT: Make sure the id supplied is unique, then take the data supplied and 
-    // instantiate the Engineer constructor.
-    
-    
-    // STUDENT: When finished:
-       // Add the new object to the team member array
-       // Pass control back to the createTeam() function
 
-  });
+        // STUDENT: When finished:
+        // Add the new object to the team member array
+        // Pass control back to the createTeam() function
+        createTeam();
+    });
 }
 
 // STUDENT: Now create a function for creating an Intern using the code above as an example
+function createIntern() {
+    inquirer.prompt([
+        // STUDENT:  Intern questions
+        {
+            type: "input",
+            name: "internName",
+            message: "What is your intern's name?",
+            // Note how the validate function works
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Please enter at least one character.";
+            }
+        },
+        {
+            type: "input",
+            name: "internId",
+            message: "What is your intern's ID?",
+            // Note how the validate function works
+            validate: answer => {
+                if (!idArray.includes(answer)) {
+                    return true;
+                }
+                return "Please enter a unique ID.";
+            }
+        },
+        {
+            type: "input",
+            name: "internEmail",
+            message: "What is your intern's email?",
+            // Note how the validate function works
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Please enter at least one character.";
+            }
+        },
+        {
+            type: "input",
+            name: "internSchool",
+            message: "What is your intern's school?",
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Please enter at least one character.";
+            }
+        }
+    ]).then(answers => {
+        idArray.push(answers.internId);
+        // STUDENT: Make sure the id supplied is unique, then take the data supplied and 
+        // instantiate the Intern constructor.
 
+
+        // STUDENT: When finished:
+        // Add the new object to the team member array
+        // Pass control back to the createTeam() function
+        createTeam();
+    });
+}
 
 // STUDENT: This function will call the render function required near the top (line 12), 
 // and pass INTO it the teamMembers area; from there, write the HTML returned back to a file 
 // in a directory called output.
-function renderHtmlPage(){
+function renderHtmlPage() {
 
 }
 
@@ -92,8 +243,8 @@ function renderHtmlPage(){
 // help keep code organized.
 function startMenu() {
 
-  // Here we start things off by calling the createManager function
-  createManager()
+    // Here we start things off by calling the createManager function
+    createManager()
 
 }
 
