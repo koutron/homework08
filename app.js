dac76eb1208f28c80f463214393381edf1450d68
Kouros <kamousavi@gmail.com>
2020-05-01 15:45:53 -0400

    added number validation to ID and Manager Office Number.

--- app.js
+++ app.js
@@ -11,14 +11,14 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 // This file will generate the final HTML. You don't need to touch this at all!
 const render = require("./lib/htmlRenderer");
 
-// This will be an array of all team member objects created
+// This will be an array of all team member objects created.
 const teamMembers = [];
 
-// This will be an array of the id values created for each object so there are no duplicates
+// This will be an array of the id values created for each object so there are no duplicates.
 const idArray = [];
 
 
-// STUDENT: This function generates all the questions for creating the manager. You need to add more to this.
+//This function generates all the questions for creating the manager.
 function createManager() {
     console.log("Please build your team");
     inquirer.prompt([
@@ -38,10 +38,11 @@ function createManager() {
             name: "managerId",
             message: "What is your manager's ID?",
             validate: answer => {
-                if (!idArray.includes(answer)) {
+                const num = parseInt(answer);
+                if (!idArray.includes(num) && !isNaN(num)) {
                     return true;
                 }
-                return "Please enter a unique ID.";
+                return "Please enter a unique numerical ID.";
             }
         },
         {
@@ -60,21 +61,22 @@ function createManager() {
             name: "managerOfficeNum",
             message: "What is your manager's office number?",
             validate: answer => {
-                if (answer !== "") {
+                const num = parseInt(answer);
+                if (!isNaN(num)) {
                     return true;
                 }
-                return "Please enter at least one character.";
+                return "Please enter a number.";
             }
         },
-        // STUDENT: Add other questions here!
-
-
     ]).then(({managerName, managerId, managerEmail, managerOfficeNum}) => {
-        // STUDENT: Process the response by instatiating a new object in the Manager class
-        idArray.push(managerId);
         
+        const managerIdNum = parseInt(managerId)
+        idArray.push(managerIdNum);
+        
+        //Process the response by instatiating a new object in the Manager class
         const manager = new Manager(managerName, managerId, managerEmail, managerOfficeNum);
         teamMembers.push(manager);
+        
         // Now call the next question set
         createTeam();
     });
@@ -82,7 +84,7 @@ function createManager() {
 
 // This function starts team creation.
 function createTeam() {
-    // STUDENT: Ask which type of team member should be created with a list of choices
+    //Ask which type of team member should be created with a list of choices
     inquirer.prompt([
         {
             type: "list",
@@ -92,8 +94,8 @@ function createTeam() {
         }
 
     ]).then(userChoice => {
-        // STUDENT: Based on which choice they make, call the correct function to ask more questions.
-        // If no choice is made, then go to the rendering function.
+        //Based on which choice they make, call the correct function to ask more questions.
+        //If no choice is made, then go to the rendering function.
         if (userChoice.teamChoice === "Engineer") {
             createEngineer();
         } else if (userChoice.teamChoice === "Intern") {
@@ -106,15 +108,14 @@ function createTeam() {
     });
 }
 
-// This function starts team creation.
+// This function starts the engineer member creation.
 function createEngineer() {
     inquirer.prompt([
-        // STUDENT:  Engineer questions
+        //Engineer questions
         {
             type: "input",
             name: "engineerName",
             message: "What is your engineer's name?",
-            // Note how the validate function works
             validate: answer => {
                 if (answer !== "") {
                     return true;
@@ -128,10 +129,11 @@ function createEngineer() {
             message: "What is your engineer's ID?",
             // Note how the validate function works
             validate: answer => {
-                if (!idArray.includes(answer)) {
+                const num = parseInt(answer);
+                if (!idArray.includes(num) && !isNaN(num)) {
                     return true;
                 }
-                return "Please enter a unique ID.";
+                return "Please enter a unique numerical ID.";
             }
         },
         {
@@ -158,27 +160,28 @@ function createEngineer() {
             }
         }
     ]).then(({engineerName, engineerId, engineerEmail, engineerGithub}) => {
-        idArray.push(engineerId);
-        // STUDENT: Make sure the id supplied is unique, then take the data supplied and 
-        // instantiate the Engineer constructor.
+        
+        const engineerIdNum = parseInt(engineerId);
+        idArray.push(engineerIdNum);
+        
+        // Add the new object to the team member array
         const engineer = new Engineer(engineerName, engineerId, engineerEmail, engineerGithub);
         teamMembers.push(engineer);
-        // STUDENT: When finished:
-        // Add the new object to the team member array
+        
+        
         // Pass control back to the createTeam() function
         createTeam();
     });
 }
 
-// STUDENT: Now create a function for creating an Intern using the code above as an example
+//Now create a function for creating an Intern
 function createIntern() {
     inquirer.prompt([
-        // STUDENT:  Intern questions
+        //Intern questions
         {
             type: "input",
             name: "internName",
             message: "What is your intern's name?",
-            // Note how the validate function works
             validate: answer => {
                 if (answer !== "") {
                     return true;
@@ -190,19 +193,18 @@ function createIntern() {
             type: "input",
             name: "internId",
             message: "What is your intern's ID?",
-            // Note how the validate function works
             validate: answer => {
-                if (!idArray.includes(answer)) {
+                const num = parseInt(answer);
+                if (!idArray.includes(num) && !isNaN(num)) {
                     return true;
                 }
-                return "Please enter a unique ID.";
+                return "Please enter a unique numerical ID.";
             }
         },
         {
             type: "input",
             name: "internEmail",
             message: "What is your intern's email?",
-            // Note how the validate function works
             validate: answer => {
                 if (answer !== "") {
                     return true;
@@ -222,20 +224,18 @@ function createIntern() {
             }
         }
     ]).then(({internName, internId, internEmail, internSchool}) => {
-        idArray.push(internId);
-        // STUDENT: Make sure the id supplied is unique, then take the data supplied and 
-        // instantiate the Intern constructor.
+        const internIdNum = parseInt(internId);
+        idArray.push(internIdNum);
+        
         const intern = new Intern(internName, internId, internEmail, internSchool);
         teamMembers.push(intern);
 
-        // STUDENT: When finished:
-        // Add the new object to the team member array
         // Pass control back to the createTeam() function
         createTeam();
     });
 }
 
-// STUDENT: This function will call the render function required near the top (line 12), 
+// This function will call the render function required near the top (line 12), 
 // and pass INTO it the teamMembers area; from there, write the HTML returned back to a file 
 // in a directory called output.
 function renderHtmlPage() {
@@ -254,13 +254,5 @@ function startMenu() {
     createManager()
 
 }
-
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
-
 
 startMenu();
